990d1b2018513750f2a7dc16ae9d2db10a9c4c90
sfger <sfger111@163.com>
2014-10-21 23:14:05 +0800

    eg. tabs

---
+++ public/js/base.js
@@ -0,0 +1,216 @@
+// light {{{
+var light = {
+	// ui {{{
+	ui:{
+		markChars:{up: '↑', down : '↓', expand:'▼', fold:'▲', close:'×', ok:'√', empty:'&nbsp;&nbsp;'}
+	},
+	// }}}
+	// util {{{
+	util:{
+		slice:Array.prototype.slice,
+		push:Array.prototype.push,
+		toString:Object.prototype.toString,
+		hasOwnProperty:Object.prototype.hasOwnProperty,
+		list2Array:function(list){
+			var ret = [], slice = light.util.slice;
+			try{
+				ret = slice.call(list);
+			}catch(e){
+				for(var i=0,ii=list.length-1; i<=ii; i++)
+					ret.push(list[i]);
+			}
+			return ret;
+		},
+		getType:function(obj){ return light.ui.toString.call(obj).slice(8, -1); },
+		isWindow:function(obj){ return obj!=null && obj==obj.window; },
+		isPlainObject:function(obj){
+			if(light.util.getType(obj)!=="object" || obj.nodeType || light.util.isWindow(obj)) return false;
+			try{
+				if(obj.constructor && !light.util.call(obj.constructor.prototype, "isPrototypeOf")) return false;
+			}catch(e){
+				return false;
+			}
+			return true;
+		},
+		// method extend {{{
+		extend:function(){
+			var i = 1,
+				target = arguments[0],
+				deep = false,
+				len = arguments.length;
+			if(typeof target=='boolean'){
+				deep = target,
+				target = arguments[i++] || {};
+			}
+			var getType = light.util.getType;
+			var targetType = getType(target);
+			if(!targetType in {"Object":1, "Function":1}) target = {};
+			if(i===len) target = {}, i--;
+			for(; i<len; i++){
+				var options = arguments[i];
+				if(options != null){
+					for(var name in options){
+						var src = target[name],
+							copy = options[name],
+							copyType = getType(copy),
+							srcType = getType(src);
+						if(target === copy) continue;
+						if(deep && copy && (light.util.isPlainObject(copy) && copyType ==='Array')){
+							var clone = (copyType==='Array')
+								? (src && (srcType==='Array') ? src : [])
+								: (src && (light.util.isPlainObject(copy)) ? src : {});
+							target[name] = light.util.extend(deep, clone, copy);
+						}else if(copy!==undefined){
+							target[name] = copy;
+						}
+					}
+				}
+			}
+			return target;
+		},
+		// }}}
+		// method createElement {{{
+		createElement:function(node){
+			var getType = light.util.getType;
+			var cd = '',
+				at=[],
+				attr = null,
+				children = null,
+				fn = light.util.createElement,
+				node_type = getType(node);
+			if(node_type === 'Array'){
+				for(var j in node) cd += fn(node[j]);
+			}else{
+				if(node_type==="String" || node_type=="Number"){
+					cd = node;
+				}else if(node_type==='Object' && node.name){
+					attr = node.attr, children = node.children, at = [];
+					if(attr){
+						for(var key in attr){
+							if(key=='className'){
+								at.push('class="' + attr[key] + '"');
+								continue;
+							}else if(key=='style'){
+								var style = attr[key];
+								var ot = getType(style);
+								attr[key] = '';
+								if(ot=='Object'){
+									for(var sk in style){
+										attr[key] += sk + ':' + style[sk] + ';';
+									}
+								}else if(ot=='String'){
+									attr[key] = style;
+								}
+							}
+							at.push('' + key + '="' + attr[key] + '"');
+						}
+					}
+					if(at.length) at.unshift('');
+					if(children && getType(children) !== 'Array') children = [children];
+					cd = '<' + node.name + at.join(' ') + '>' + (children ? fn(children) : '') + '</' + node.name + '>';
+				} else cd = '';
+			}
+			return cd;
+		},
+		// }}}
+		// method createDomElement {{{
+		createDomElement:function(node, container){
+			if(!node) return;
+			var getType = light.util.getType;
+			var ret = container ? container : document.createDocumentFragment(),
+				fn = light.util.createDomElement,
+				node_type = getType(node),
+				text = {String:1, Number:1},
+				hasOwnProperty = Object.prototype.hasOwnProperty;
+			if(text[node_type]){
+				ret.appendChild(document.createTextNode(node));
+			}else if(node_type==='Object' && node.name){
+				var attr = node.attr, prop = node.prop, children = node.children, key;
+				if(attr){
+					for(key in attr){
+						if(key==='style'){
+							var style = attr[key], ot = getType(style);
+							if(ot=='Object'){
+								for(var sk in style)
+									if(hasOwnProperty.call(style, sk))
+										ret.style[sk] = style[sk];
+							}else if(ot=='String'){
+								ret.style.cssText = style;
+							}
+						}else{
+							ret.setAttribute(key, attr[key]);
+						}
+					}
+				}
+				if(prop){
+					for(key in prop){
+						if(hasOwnProperty.call(prop, key)){
+							if(key=='class') key = 'className';
+							ret[key] = prop[key];
+						}
+					}
+				}
+				if(children){
+					if(text[getType(children)]){
+						ret.innerHTML += children;
+					}else{
+						if(getType(children)!=='Array') children = children[i];
+						var sub;
+						for(var i=0,ii=children.length-1; i<=ii; i++){
+							if(text[getType(children[i])]){
+								ret.innerHTML += children[i];
+							}else if(children[i].name){
+								sub = document.createElement(children[i].name);
+								ret.appendChild(sub);
+								fn(children[i], sub);
+							}
+						}
+					}
+				}
+			}
+			if(!ret.children) ret.children = ret.childNodes;
+			ret = null;
+		}
+		// }}}
+	},
+	// }}}
+	// Event {{{
+	Event: {
+		on:function(eventType, target, fn, useCapture){
+			var a = function(eventType, bindFn, useCapture){
+				var w3c = window.addEventListener ? true : false;
+				this[w3c ? 'addEventListener' : 'attachEvent'](
+					(w3c ? '': 'on') + eventType,
+					bindFn,
+					useCapture
+				);
+			};
+			fn.bindEventListener = function(e){ fn.call(e.target||e.srcElement, e||event); };
+			a.apply(target, [
+				eventType,
+				fn.bindEventListener,
+				(useCapture === undefined) ? true : useCapture
+			]);
+		},
+		off:function(eventType, target, fn, useCapture){
+			var w3c = window.removeEventListener ? true : false;
+			target[w3c ? 'removeEventListener' : 'detachEvent'](
+				(w3c ? '': 'on') + eventType,
+				fn.bindEventListener,
+				(useCapture === undefined) ? true : useCapture
+			);
+		},
+		stop:function(e){
+			if(e.stopPropagation) e.stopPropagation();
+			else e.cancelBubble = true;
+		},
+		prevent:function(e){
+			if(e.preventDefault) e.preventDefault();
+			else e.returnValue = false;
+		}
+	}
+	// }}}
+};
+// }}}
+
+/* vim: set fdm=marker : */
---
+++ public/js/jquery.tabs.js
@@ -0,0 +1,142 @@
+"use strict";
+(function($){
+$.fn.tabs=function(options){
+	if(!options){
+		var iTabs = [];
+		this.each(function(){
+			if(this.ui && this.ui.iTab) iTabs.push(this.ui.iTab);
+			else throw new Error('UI does not init...');
+		});
+		return iTabs;
+	}
+	options = $.extend(true, {
+		width:900,
+		height:80,
+		tabWidth:160,
+		border:true,
+		icon:null,
+		selected:0,
+		position:'north'
+	}, options);
+	var slice = light.util.slice;
+	var createElement = light.util.createElement;
+	options.renders = slice.call(this);
+	var handler = function(box, options){ return new handler.prototype.init(box, options); };
+	var list2Array = light.util.list2Array;
+	handler.prototype = {
+		init: function(box, options){
+			this.render = box;
+			this.userOptions = options;
+			this.headers = list2Array(box.children[0].children);
+			this.panels = list2Array(box.children[1].children);
+			var that = this;
+			var $box = $(box);
+			$(box.children[0]).addClass('cf');
+			$box.addClass('tab-container');
+			if(this.headers.length){
+				$(this.headers[options.selected]).addClass('current');
+				$(this.panels).parent().show().end().hide().eq(options.selected).show();
+			}
+			if(options.contentFit){
+				$(window).resize(function(){
+					box.children[1].style.height = (box.parentNode.offsetHeight - box.children[0].offsetHeight - 1) + 'px';
+				}).resize();
+			}
+			$(box.children[0]).delegate('li', {
+				click:function(e){
+					that.select(that.headers.indexOf(this));
+				}
+			}).delegate('.closer', {
+				click:function(e){
+					that.close(that.headers.indexOf(this.parentNode.parentNode));
+					return false;
+				}
+			});
+		},
+		add:function(op, index){
+			var render = this.render;
+			var len = this.headers.length;
+			var position = 'beforeBegin';
+			if(index===undefined) index = len;
+			if(index<0) index = 0;
+			if(!len){
+				index = 0;
+				$(render.children[1]).show();
+			}else if(index>=len){
+				index = len;
+				position = 'afterEnd';
+			}
+			var header = createElement({
+				name:'li', children:{
+					name:'a', attr:{href:'javascript:;'}, children:
+						(function(){
+							var ret = ['<span class="title">'+op.title+'</span>'];
+							if(op.icon){
+								ret.unshift(createElement({
+									name:'span', attr:{className:'icon icon-'+op.icon}
+								}));
+							}
+							if(op.closable){
+								ret.push(createElement({
+									name:'span', attr:{className:'closer'}, children:
+										light.ui.markChars.close
+								}));
+							}
+							return ret;
+						})()
+				}
+			});
+			var panel = createElement({name:'div', attr:{className:'tab-content', style:{display:'none'}}, children:op.content});
+			if(this.headers.length){
+				var i = position==='afterEnd' ? index -1 : index;
+				this.headers[i].insertAdjacentHTML(position, header);
+				this.panels[i].insertAdjacentHTML(position, panel);
+			}else{
+				render.children[0].innerHTML = header;
+				render.children[1].innerHTML = panel;
+			}
+			this.headers = list2Array(render.children[0].children);
+			this.panels = list2Array(render.children[1].children);
+			if(index<=this.userOptions.selected) this.userOptions.selected++;
+			if(op.select) this.select(index);
+			var box = this.render;
+			box.children[1].style.height = (box.parentNode.offsetHeight - box.children[0].offsetHeight - 1) + 'px';
+			return this;
+		},
+		close: function(index){
+			var header = this.headers.splice(index, 1)[0];
+			var panel = this.panels.splice(index, 1)[0];
+			header.parentNode.removeChild(header);
+			panel.parentNode.removeChild(panel);
+			var options = this.userOptions;
+			if(options.selected==index){
+				if(this.headers.length){
+					this.select(index-1>0 ? index - 1 : 0);
+				}else{
+					options.selected = null;
+				}
+			}else if(options.selected>index){
+				options.selected--;
+			}
+			return this;
+		},
+		select: function(index){
+			var prevSelected = this.userOptions.selected;
+			if(index<0 || index>this.headers.length-1) return false;
+			$(this.headers[prevSelected]).removeClass('current');
+			$(this.headers[index]).addClass('current');
+			$(this.panels[prevSelected]).hide();
+			$(this.panels[index]).show();
+			this.userOptions.selected = index;
+			return this;
+		}
+	};
+	handler.prototype.init.prototype = handler.prototype;
+	return this.each(function(){
+		this.ui = {
+			iTab: handler(this, $.extend({}, options))
+		}
+	});
+};
+})(jQuery);
+/* vim: set fdm=marker : */
