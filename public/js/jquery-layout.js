9d9a939a4129dcd122b5f40314de019901fbb478
sfger <sfger111@163.com>
2014-11-20 00:12:48 +0800

    layout fixed

--- public/js/jquery-layout.js
+++ public/js/jquery-layout.js
@@ -4,8 +4,12 @@ $.fn.layout=function(options){
 	if(!options){
 		var iLayouts = [];
 		this.each(function(){
-			if(this.ui && this.ui.iLayout) iLayouts.push(this.ui.iLayout);
-			else throw new Error('UI does not init...');
+			var ui = $(this).data('ui');
+			if(ui && ui.iLayout){
+				iLayouts.push(ui.iLayout);
+			}else{
+				throw new Error('UI does not init...');
+			}
 		});
 		return iLayouts;
 	}
@@ -38,17 +42,17 @@ $.fn.layout=function(options){
 			this.box = box;
 			this.userOptions = options;
 			this.panels = {
-				north: $('.layout-north', box).get(0),
-				south: $('.layout-south', box).get(0),
-				west: $('.layout-west', box).get(0),
-				east: $('.layout-east', box).get(0),
-				center: $('.layout-center', box).get(0)
+				north  : $('.layout-north', box).get(0),
+				south  : $('.layout-south', box).get(0),
+				west   : $('.layout-west', box).get(0),
+				east   : $('.layout-east', box).get(0),
+				center : $('.layout-center', box).get(0)
 			};
 			this.panelBars = {
-				north: $('.bar-north', box).css({height:options.panelBar.each.north.height||options.panelBar.size}).get(0),
-				south: $('.bar-south', box).css({height:options.panelBar.each.south.height||options.panelBar.size}).get(0),
-				west: $('.bar-west', box).css({width:options.panelBar.each.west.width||options.panelBar.size}).get(0),
-				east: $('.bar-east', box).css({width:options.panelBar.each.east.width||options.panelBar.size}).get(0)
+				north : $('.bar-north', box).css({height : options.panelBar.each.north.height||options.panelBar.size}).get(0),
+				south : $('.bar-south', box).css({height : options.panelBar.each.south.height||options.panelBar.size}).get(0),
+				west  : $('.bar-west', box).css({width   : options.panelBar.each.west.width||options.panelBar.size}).get(0),
+				east  : $('.bar-east', box).css({width   : options.panelBar.each.east.width||options.panelBar.size}).get(0)
 			};
 			var that = this;
 			$(['Height', 'Width']).each(function(i, one){
@@ -65,7 +69,7 @@ $.fn.layout=function(options){
 			});
 			if(options.panel.resize) this.panelResize();
 			if(options.panel.toggle) this.panelToggle();
-			$(window).resize(function(){that.resize()});
+			$(window).resize(function(){that.resize();});
 			this.resize();
 		},
 		disableSelection: function(){
@@ -93,28 +97,28 @@ $.fn.layout=function(options){
 					var $cover = box.cover ? $(box.cover).show() : $('<div></div>').appendTo(document.body);
 					box.cover = $cover.get(0);
 					$cover.css({
-						position:'absolute',
-						opacity: 0.1,
-						filter:'alpha(opacity=10)',
-						background:'white',
-						zIndex:50,
-						top:0,
-						left:0,
-						width: that.getViewWidth(),
-						height: that.getViewHeight()
+						position   : 'absolute',
+						opacity    : 0.1,
+						filter     : 'alpha(opacity=10)',
+						background : 'white',
+						zIndex     : 50,
+						top        : 0,
+						left       : 0,
+						width      : that.getViewWidth(),
+						height     : that.getViewHeight()
 					});
 					var $proxy = this.proxy ? $(this.proxy).show() : $(this).clone().html('').appendTo(document.body);
 					this.proxy = $proxy.get(0);
 					$proxy.css({
-						position:'absolute',
-						opacity:0.5,
-						filter:'alpha(opacity=50)',
-						zIndex:100,
-						width:this.offsetWidth,
-						height:this.offsetHeight,
-						top:$(this).position().top,
-						left:$(this).position().left,
-						background:'black'
+						position   : 'absolute',
+						opacity    : 0.5,
+						filter     : 'alpha(opacity=50)',
+						zIndex     : 100,
+						width      : this.offsetWidth,
+						height     : this.offsetHeight,
+						top        : $(this).position().top,
+						left       : $(this).position().left,
+						background : 'black'
 					});
 					var document_events = {
 						'mousemove': function(e){
@@ -127,10 +131,15 @@ $.fn.layout=function(options){
 							var x = e.pageX, y = e.pageY;
 							var barSize = that.userOptions.panelBar.size;
 							var barOps = that.userOptions.panelBar.each;
-							if($bar.hasClass('bar-west')) resize_box.css({width:x});
-							else if($bar.hasClass('bar-east')) resize_box.css({width:that.getViewWidth()-x-(barOps.east.width||barSize)});
-							else if($bar.hasClass('bar-north')) resize_box.css({height:y});
-							else if($bar.hasClass('bar-south')) resize_box.css({height:that.getViewHeight()-y-(barOps.south.height||barSize)});
+							if($bar.hasClass('bar-west')){
+								resize_box.css({width:x});
+							}else if($bar.hasClass('bar-east')){
+								resize_box.css({width:that.getViewWidth()-x-(barOps.east.width||barSize)});
+							}else if($bar.hasClass('bar-north')){
+								resize_box.css({height:y});
+							}else if($bar.hasClass('bar-south')){
+								resize_box.css({height:that.getViewHeight()-y-(barOps.south.height||barSize)});
+							}
 							if(!resize_box.get(0).offsetHeight || !resize_box.get(0).offsetWidth){
 								resize_box.hide();
 								$bar.get(0).style.cursor = 'auto';
@@ -147,9 +156,9 @@ $.fn.layout=function(options){
 		panelToggle: function(){
 			var that = this;
 			$('.resize-bar span', this.box).click(function(e){
-				var $bar = $(this).parent();
-				var $toggle_box = $bar.hasClass('bar-south') ? $bar.next() : $bar.prev();
-				var pops = that.userOptions.panel.each;
+				var $bar = $(this).parent(),
+					$toggle_box = $bar.hasClass('bar-south') ? $bar.next() : $bar.prev(),
+					pops = that.userOptions.panel.each;
 				if( $bar.hasClass('bar-north')&&pops.north.toggle===false
 					|| $bar.hasClass('bar-south')&&pops.south.toggle===false
 					|| $bar.hasClass('bar-west')&&pops.west.toggle===false
@@ -171,38 +180,40 @@ $.fn.layout=function(options){
 			});
 		},
 		resize: function(){
-			var getElementHeight = this.getElementHeight;
-			var panels = this.panels;
-			var panelBars = this.panelBars;
-			var md_container_height = this.getViewHeight()
-				- getElementHeight(panels.north)
-				- getElementHeight(panels.south)
-				- getElementHeight(panelBars.north)
-				- getElementHeight(panelBars.south);
-			$('.layout-middle-container', this.box).height(md_container_height)
-				.find('.resize-bar').css({lineHeight:md_container_height+'px'});
-			var doc_mode = document.documentMode;
+			var getElementHeight    = this.getElementHeight,
+				panels              = this.panels,
+				panelBars           = this.panelBars,
+				ie6                 = /MSIE 6/,
+				doc_mode            = document.documentMode,
+				md_container_height = this.getViewHeight()
+									- getElementHeight(panels.north)
+									- getElementHeight(panels.south)
+									- getElementHeight(panelBars.north)
+									- getElementHeight(panelBars.south);
+
+			$('.layout-middle-container', this.box).height(md_container_height);
+				// .find('.resize-bar').css({lineHeight:md_container_height+'px'});
 			// if(doc_mode<=8 || /MSIE 7/.test(navigator.userAgent) || /MSIE 6/.test(navigator.userAgent)){
-				$('.layout-middle-container .resize-bar', this.box).css({position:'relative'})
-					.find('span').css({position:'absolute', top:md_container_height/2-10, left:0});
+				// $('.layout-middle-container .resize-bar', this.box).css({position:'relative'})
+				// 	.find('span').css({position:'absolute', top:md_container_height/2-10, left:0});
 			// }
-			if(doc_mode===5 || /MSIE 6/.test(navigator.userAgent)){
-				var getElementWidth = this.getElementWidth;
-				var center_width = this.getViewWidth()
-					- getElementWidth(panels.west)
-					- getElementWidth(panels.east)
-					- getElementWidth(panelBars.west)
-					- getElementWidth(panelBars.east);
-				$('.layout-center', this.box).css({float:'left', width:center_width, height:md_container_height});
+			if(doc_mode===5 || ie6.test(navigator.userAgent)){
+				var getElementWidth = this.getElementWidth,
+					center_width    = this.getViewWidth()
+									- getElementWidth(panels.west)
+									- getElementWidth(panels.east)
+									- getElementWidth(panelBars.west)
+									- getElementWidth(panelBars.east);
+				$('.layout-center', this.box).css({'float':'left', 'width':center_width, 'height':md_container_height});
 				$('.layout-middle-container>div', this.box).height(md_container_height);
 			}
 		}
 	};
 	handler.prototype.init.prototype = handler.prototype;
 	return this.each(function(){
-		this.ui = {
+		$(this).data('ui', {
 			iLayout: handler(this, $.extend({}, options))
-		}
+		});
 	});
 };
 })(jQuery);
