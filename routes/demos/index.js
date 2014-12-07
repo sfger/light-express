383196e509c96bca7a62aa82442da647ce74a4db
sfger <sfger111@163.com>
2014-12-08 00:35:03 +0800

    404

--- routes/demos/index.js
+++ routes/demos/index.js
@@ -1,15 +1,24 @@
 var express = require('express');
-var router = express.Router();
+var path    = require('path');
+var fs      = require('fs');
+var router  = express.Router();
 
 var dir = 'demos/';
 var ctrl = dir + 'index';
-router.get('/' + ctrl, function(req, res) {
-	var view = dir + req.query.view;
-	// console.log(view);
-	res.render(view, {
-		dir:dir,
-		___: req.query.dist == 1 ? view.replace(/[^\/]+/g, '..') : ''
-	}, express.UserConfig.dist.bind({req:req, res:res, distPath:view}));
+router.get(['/demos/index', '/demos/', '/demos'], function(req, res) {
+	var view = path.normalize(dir + req.query.view);
+	var view_path = path.normalize(process.cwd() + '/views/' + view + '.ejs');
+	fs.exists(view_path, function(exists){
+		if(exists){
+			res.render(view, {
+				dir:dir,
+				___: req.query.dist == 1 ? view.replace(/[^\/]+/g, '..') : ''
+			}, express.UserConfig.dist.bind({req:req, res:res, distPath:view}));
+		}else{
+			res.writeHead(404, {"Content-Type": 'Not Found'});
+			res.end('<h1>404 Not Found<h1>');
+		}
+	});
 });
 
 module.exports = router;
