7b2316cc37fd50751e14fb0ba98e2858bc5a3bc5
sfger <sfger111@163.com>
2016-03-17 13:39:07 +0800

    update route

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var express              = require('express');
 var fs                   = require('fs');
 var path                 = require('path');
 var libUrl               = require('url');
-// var favicon           = require('static-favicon');
 var logger               = require('morgan');
 var cookieParser         = require('cookie-parser');
 var session              = require('cookie-session');
@@ -19,38 +18,33 @@ app.use(webpackDevMiddleware(compiler, {
 express.UserConfig = require('./config');
 app.set('views', path.join(__dirname, '/public'));
 app.set('view engine', 'ejs');
-// app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
-// if(isDev) app.use(require('node-compass')({mode:'compressed', css:'css', sass:'scss', img:'img'}));
 if(isDev){
 	app.use(express.UserConfig.compileSCSS);
 	app.use(express.UserConfig.compileTemplate);
 }
 app.use(express.UserConfig.staticHttpCombo);
-app.use(express['static'](path.join(__dirname, express.UserConfig.staticDir), {index:'______.html'}));
 app.locals.__version__ = '20150821017';
 app.use(session({name:'_SSID_', keys:['skey1', 'skey2']}));
 
 Promise.resolve(new Promise(function(resolve, reject){
 	express.UserConfig.autoAddRoutes(app, path.join(__dirname, 'routes'), '/', {resolve:resolve, reject:reject});
 })).then(function(){
-	/// catch 404 and forward to error handler
+	app.use(express['static'](path.join(__dirname, express.UserConfig.staticDir), {index:'______.html'}));
 	app.use(function(req, res, next) {
 		var err = new Error('Not Found');
 		err.status = 404;
 		next(err);
 	});
-
-	/// error handlers
 	app.use(function(err, req, res, next) {
 		var status = err.status || 500;
 		res.status(status);
 		res.render(status, {
-			message: err.message,
-			error: {}
+			message:err.message,
+			error:{}
 		});
 	});
 });
--- routes/index.js
+++ routes/index.js
@@ -3,47 +3,42 @@ var libFs	= require('fs');
 var router  = express.Router();
 var path    = require('path');
 
-router.get(['/*'], function(req, res, next){
+router.get(['/*.html'], function(req, res, next){
 	var app      = req.app;
-	var url_path = req.path.slice(1);
+	var url_path = req.path.slice(1, -5);
 	if(!url_path) url_path = 'index';
 	var view     = url_path.replace(/\/html\//, '/views/');
 	var file     = path.join(app.get('views'), view) + '.ejs';
 
 	file = path.normalize(file);
 	libFs.exists(file, function(exists){
-		if(!exists){
-			next();
-			// res.writeHead(404, {"Content-Type":"text/html"} );
-			// res.end( "<h1>404 Not Found</h1>" );
-		}else{
-			res.render(view, {
-				___: '',
-				node: {
-					placeholder:{
-						white:"/common/img/placeholder.png"
-					},
-					get_img: function(width, height, bg, color, text){
-						var colorTrans = function(c){
-							return c.replace(/./g, function(s){return s+s;});
-						};
-						height = height || width;
-						bg     = bg     || 'ccc';
-						color  = color  || '000';
-						text   = text   || (width + 'X' + height);
+		if(!exists) return next();
+		res.render(view, {
+			___: '',
+			node: {
+				placeholder:{
+					white:"/common/img/placeholder.png"
+				},
+				get_img: function(width, height, bg, color, text){
+					var colorTrans = function(c){
+						return c.replace(/./g, function(s){return s+s;});
+					};
+					height = height || width;
+					bg     = bg     || 'ccc';
+					color  = color  || '000';
+					text   = text   || (width + 'X' + height);
 
-						// return 'http://dummyimage.com/'+width+'x'+height+'/'+bg+'/'+color+'.png&text=' + text;
-						// return 'http://fakeimg.pl/'+width+'x'+height+'/'+bg+'/'+color+'/?text=' + text;
+					// return 'http://dummyimage.com/'+width+'x'+height+'/'+bg+'/'+color+'.png&text=' + text;
+					// return 'http://fakeimg.pl/'+width+'x'+height+'/'+bg+'/'+color+'/?text=' + text;
 
-						// 不支持三位颜色的，变换成六位
-						if(bg.length==3) bg = colorTrans(bg);
-						if(color.length==3) color = colorTrans(color);
+					// 不支持三位颜色的，变换成六位
+					if(bg.length==3) bg = colorTrans(bg);
+					if(color.length==3) color = colorTrans(color);
 
-						return 'http://fpoimg.com/'+width+'x'+height+'?bg_color='+bg+'&text_color='+color+'&text=' + text;
-					}
+					return 'http://fpoimg.com/'+width+'x'+height+'?bg_color='+bg+'&text_color='+color+'&text=' + text;
 				}
-			}, express.UserConfig.dist.bind({req:req, res:res, distPath:url_path}));
-		}
+			}
+		}, express.UserConfig.dist.bind({req:req, res:res, distPath:url_path}));
 	});
 });
 
