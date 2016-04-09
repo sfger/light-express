a9575879f9732e7d9fb5bc1cd3ba153b19a97604
sfger <sfger111@163.com>
2016-04-09 23:14:27 +0800

    update css

--- public/sfger/js/main.js
+++ public/sfger/js/main.js
@@ -68,15 +68,18 @@ require([
 		global_router: function(){
 			router = this.router;
 			router.on('on', '/?(.*)', function(path, next){
-				if(app.page) app.page.destroy();
+				var destroy_previous_page = function(){
+					if(app.page) app.page.destroy();
+					$(app.root).empty();
+				};
 				new Promise(function(resolve, reject){
 					var module = 'sfger/js/' + (path||'index');
 					require([module], resolve, function(err){
 						return reject(404);
 					});
 				}).then(function(page){
+					destroy_previous_page();
 					if(!page) return Promise.reject(404);
-					$(app.root).empty();
 					page.option = {
 						router : router,
 						path   : path,
@@ -86,15 +89,13 @@ require([
 					page.create();
 					app.page = page;
 				})['catch'](function(err){
+					destroy_previous_page();
 					router.dispatch('on', '/404');
 				});
 			});
 		},
 		config_router: function(){
 			router.on('on', '/article/:index', function(article_index, next){
-				// var base_css = document.getElementById('css-base');
-				// base_css.removeAttribute('disabled');
-				// base_css.setAttribute('disabled', true);
 				require.config({
 					shim: {
 						'public/highlight':{exports:'hljs'}
@@ -109,7 +110,7 @@ require([
 					var md = new markdown().set({html:true, breaks:true});
 					article = md.render(article[article_index+'.markdown']);
 					new EJS({
-						text: '<div class="article-page"><div class="article-bar article-header"><a href="#/index">首页</a> <a href="<%- home %>/index.html#/article/markdown">本文链接</a></div><div class="markdown-body md-ctn"><%=data%></div><div class="article-bar article-footer"><a href="#/index">首页</a> <a href="<%- home %>/index.html#/article/markdown">本文链接</a></div></div>'
+						text: '<div class="article-page"><div class="article-bar article-header"><div class="wrap"><a href="#/index">首页</a> <a href="#/article/markdown">本文链接</a></div></div><div class="markdown-body md-ctn wrap"><%=data%></div><div class="article-bar article-footer"><div class="wrap"><a href="#/index">首页</a> <a href="/#/article/markdown">本文链接</a></div></div></div>'
 					}).update(app.root, {
 						data: article,
 						home: '#/index'
