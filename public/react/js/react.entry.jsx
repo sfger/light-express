8f8fbee02f6493a4ab3430cd0d89574640687927
sfger <sfger111@163.com>
2016-04-15 18:16:07 +0800

    entery fix

--- public/react/js/react.entry.jsx
+++ public/react/js/react.entry.jsx
@@ -9,18 +9,20 @@ var CommentBox = React.createClass({
 			count: this.state.count + 1,
 		}, function(){
 			console.log(222,this.state.count);
+			console.log(this.props.a);
 		});
 		console.log(111,this.state.count);
 	},
 	render: function() {
 		return (
 			<div className="commentBox" onClick={this.test}>
-				Hello, world! I am a CommentBox.
+				Hello, world! I am a CommentBox. Counter: {this.state.count}
 			</div>
 		);
 	}
 });
+var test = {a:'aaa', b:'bbb'};
 ReactDOM.render(
-	<CommentBox />,
+	<CommentBox data-row={1} {...test} />,
 	document.querySelector('#page')
 );
