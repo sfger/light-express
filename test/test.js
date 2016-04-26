8bca62253a4133156c6e4cbf5035460f036c5f53
sfger <sfger111@163.com>
2016-04-26 17:45:55 +0800

    update test case

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 process.chdir('../');
 var Extension = require('../extension');
-Extension.CompileDir2JS('/sfger/htpl/').then(function(data){
+Extension.CompileDir2JS('/share/htpl/').then(function(data){
 	console.log("parsed:\n\t"+data.join("\n\t"));
 });
