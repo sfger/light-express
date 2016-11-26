7be0cade0da67b6a88edc2a58b6f18b8da9c7c58
sfger <sfger111@163.com>
2016-11-26 17:30:50 +0800

    use vw mobile

--- public/public/js/fn.js
+++ public/public/js/fn.js
@@ -245,6 +245,27 @@ function domCountDown(option){//{{{
 // 		dom_node.innerHTML = '结束';
 // 	}
 // });//}}}
+function throttle(fn, delay, mustRunDelay){//{{{
+	var timer = null;
+	var t_start;
+	return function() {
+		var context = this,
+			args = arguments,
+			t_curr = +new Date();
+		clearTimeout(timer);
+		if (!t_start) {
+			t_start = t_curr;
+		}
+		if (t_curr - t_start >= mustRunDelay) {
+			fn.apply(context, args);
+			t_start = t_curr;
+		} else {
+			timer = setTimeout(function() {
+				fn.apply(context, args);
+			}, delay);
+		}
+	};
+}//}}}
 
 function isPlainObject(obj){//{{{
 	if(getType(obj)!=="object" || obj.nodeType || isWindow(obj)) return false;
