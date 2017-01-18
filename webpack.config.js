453cc7de46463efbf25d91ac6cc03edae8aeb8b7
sfger <sfger111@163.com>
2017-01-18 22:53:48 +0800

    upgrade

--- webpack.config.js
+++ webpack.config.js
@@ -61,29 +61,6 @@ module.exports = {
 				loader:'babel-loader',
 				query:{
 					presets:['es2015', 'react']
-					// presets:['babili']
-					// plugins: [
-					// 	'transform-es2015-template-literals',
-					// 	'transform-es2015-literals',
-					// 	'transform-es2015-function-name',
-					// 	'transform-es2015-arrow-functions',
-					// 	'transform-es2015-block-scoped-functions',
-					// 	'transform-es2015-classes',
-					// 	'transform-es2015-object-super',
-					// 	'transform-es2015-shorthand-properties',
-					// 	'transform-es2015-computed-properties',
-					// 	'transform-es2015-for-of',
-					// 	'transform-es2015-sticky-regex',
-					// 	'transform-es2015-unicode-regex',
-					// 	'check-es2015-constants',
-					// 	'transform-es2015-spread',
-					// 	'transform-es2015-parameters',
-					// 	'transform-es2015-destructuring',
-					// 	'transform-es2015-block-scoping',
-					// 	'transform-es2015-typeof-symbol',
-					// 	['transform-regenerator', { async: false, asyncGenerators: false }],
-					// 	"transform-es3-property-literals"
-					// ],
 				}
 			},
 			{test:/\.scss/, loader:'style-loader!css-loader!sass-loader'},
