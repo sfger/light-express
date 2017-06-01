8db0bb0ef1df8c0493c5e843c64fbd15fbe2f55b
sfger <sfger111@163.com>
2017-06-01 19:46:54 +0800

    rxjs

---
+++ public/rxjs/index.entry.ts
@@ -0,0 +1,36 @@
+// var Rx = require('rxjs');
+// import * as Rx from 'rxjs';
+var increaseButton = document.querySelector('#increase');
+var increase = Rx.Observable.fromEvent(increaseButton, 'click').startWith(0)
+.map(() => state => Object.assign({}, state, {count: state.count + 1}));
+
+var decreaseButton = document.querySelector('#decrease');
+var decrease = Rx.Observable.fromEvent(decreaseButton, 'click').startWith(0)
+.map(() => state => Object.assign({}, state, {count: state.count - 1}));
+
+var inputElement = document.querySelector('#input');
+var input = Rx.Observable.fromEvent(inputElement, 'input').startWith({target:{value:inputElement.value}})
+.map(event => state => Object.assign({}, state, {inputValue: event.target.value}));
+var textchange = Rx.Observable.fromEvent(inputElement, 'textchange')
+.map(event => state => Object.assign({}, state, {inputValue: event.target.value}));
+
+var state = Rx.Observable.merge(increase, decrease, input, textchange).scan((state, changeFn) => changeFn(state), {
+	count: 0,
+	inputValue: ''
+});
+
+// state.subscribe((state) => {
+// 	document.querySelector('#count').innerHTML = state.count;
+// 	document.querySelector('#hello').innerHTML = 'Hello ' + state.inputValue;
+// });
+
+var prevState = {count:0, inputValue:''};
+state.subscribe((state) => {
+	if (state.count !== prevState.count) {
+		document.querySelector('#count').innerHTML = state.count;
+	}
+	if (state.inputValue !== prevState.inputValue) {
+		document.querySelector('#hello').innerHTML = 'Hello ' + state.inputValue;
+	}
+	prevState = state;
+});
