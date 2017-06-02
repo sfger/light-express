af82224a42ea72dd2b43312c338c8f195e5d6b36
sfger <sfger111@163.com>
2017-06-02 22:38:01 +0800

    udpate

--- public/rxjs/index.entry.ts
+++ public/rxjs/index.entry.ts
@@ -1,5 +1,6 @@
 // var Rx = require('rxjs');
 // import * as Rx from 'rxjs';
+declare var Rx:any;
 var increaseButton = document.querySelector('#increase');
 var increase = Rx.Observable.fromEvent(increaseButton, 'click').startWith(0)
 .map(() => state => Object.assign({}, state, {count: state.count + 1}));
@@ -8,7 +9,7 @@ var decreaseButton = document.querySelector('#decrease');
 var decrease = Rx.Observable.fromEvent(decreaseButton, 'click').startWith(0)
 .map(() => state => Object.assign({}, state, {count: state.count - 1}));
 
-var inputElement = document.querySelector('#input');
+var inputElement:any = document.querySelector('#input');
 var input = Rx.Observable.fromEvent(inputElement, 'input').startWith({target:{value:inputElement.value}})
 .map(event => state => Object.assign({}, state, {inputValue: event.target.value}));
 var textchange = Rx.Observable.fromEvent(inputElement, 'textchange')
