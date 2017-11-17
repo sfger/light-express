ed2cf0271f6ccb6872588859d92a192657698561
sfger <sfger111@163.com>
2017-11-17 14:38:19 +0800

    update

--- components/fn.js
+++ components/fn.js
@@ -248,26 +248,103 @@ function domCountDown(option){
 // 		dom_node.innerHTML = '结束';
 // 	}
 // });
-function throttle(fn, delay, mustRunDelay){
-	var timer = null;
-	var t_start;
-	return function() {
-		var context = this,
-			args = arguments,
-			t_curr = +new Date();
-		clearTimeout(timer);
-		if (!t_start) {
-			t_start = t_curr;
+/* debounce 和 throttle 来自underscore
+ * */
+function restArgs(func, startIndex) {
+	startIndex = startIndex==null ? func.length-1 : +startIndex;
+	return function(){
+		var length = Math.max(arguments.length - startIndex, 0),
+			rest = Array(length),
+			index = 0;
+		for (; index < length; index++) {
+			rest[index] = arguments[index + startIndex];
 		}
-		if (t_curr - t_start >= mustRunDelay) {
-			fn.apply(context, args);
-			t_start = t_curr;
-		} else {
-			timer = setTimeout(function() {
-				fn.apply(context, args);
-			}, delay);
+		switch (startIndex) {
+			case 0: return func.call(this, rest);
+			case 1: return func.call(this, arguments[0], rest);
+			case 2: return func.call(this, arguments[0], arguments[1], rest);
 		}
+		var args = Array(startIndex + 1);
+		for (index = 0; index < startIndex; index++) {
+			args[index] = arguments[index];
+		}
+		args[startIndex] = rest;
+		return func.apply(this, args);
 	};
+}
+var delay = restArgs(function(func, wait, args) {
+	return setTimeout(function() {
+		return func.apply(null, args);
+	}, wait);
+});
+function debounce(func, wait, immediate) {
+	var timeout, result;
+
+	var later = function(context, args) {
+		timeout = null;
+		if (args) result = func.apply(context, args);
+	};
+
+	var debounced = restArgs(function(args) {
+		if (timeout) clearTimeout(timeout);
+		if (immediate) {
+			var callNow = !timeout;
+			timeout = setTimeout(later, wait);
+			if (callNow) result = func.apply(this, args);
+		} else {
+			timeout = delay(later, wait, this, args);
+		}
+
+		return result;
+	});
+
+	debounced.cancel = function() {
+		clearTimeout(timeout);
+		timeout = null;
+	};
+
+	return debounced;
+}
+
+function throttle(func, wait, options) {
+	var timeout, context, args, result;
+	var previous = 0;
+	if (!options) options = {};
+
+	var later = function() {
+		previous = options.leading === false ? 0 : Date.now();
+		timeout = null;
+		result = func.apply(context, args);
+		if (!timeout) context = args = null;
+	};
+
+	var throttled = function() {
+		var now = Date.now();
+		if (!previous && options.leading === false) previous = now;
+		var remaining = wait - (now - previous);
+		context = this;
+		args = arguments;
+		if (remaining <= 0 || remaining > wait) {
+			if (timeout) {
+				clearTimeout(timeout);
+				timeout = null;
+			}
+			previous = now;
+			result = func.apply(context, args);
+			if (!timeout) context = args = null;
+		} else if (!timeout && options.trailing !== false) {
+			timeout = setTimeout(later, remaining);
+		}
+		return result;
+	};
+
+	throttled.cancel = function() {
+		clearTimeout(timeout);
+		previous = 0;
+		timeout = context = args = null;
+	};
+
+	return throttled;
 }
 
 function isPlainObject(obj){
@@ -353,6 +430,6 @@ export{
 	strtotime, pad, date, getType, createElement,
 	numberFormat, numberFromFormat,
 	buildRegString, isStrHasTrunkWith,
-	domCountDown, throttle, isPlainObject,
+	domCountDown, debounce, throttle, isPlainObject,
 	extend, isWindow, getCurrentScript
 };
