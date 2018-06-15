8565974f12cf2c4afe7bf383981ecfdba48401a7
sfger <sfger111@163.com>
2018-06-15 15:21:55 +0800

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,48 +1,48 @@
 module.exports = {
-	"env"          : {
-		"browser"  : true,
-		"commonjs" : true,
-		"jquery"   : true,
-		"amd"      : true,
-		"es6"      : true,
-		"node"     : true
-	},
-	"extends": [
-		"eslint:recommended",
-		"plugin:react/recommended"
-	],
-	"parser": "babel-eslint",
-	"parserOptions": {
-		"ecmaVersion": 8, // or 7
-		"ecmaFeatures": {
-			"jsx": true,
-			"experimentalObjectRestSpread": true
-		},
-		"sourceType": "module"
-	},
-	"plugins" : [
-		'markdown',
-		'react',
-		'babel'
-	],
-	"globals" : {
-		"window"                  : true,
-		"EJS"                     : true,
-		"undefined"               : true,
-		"light"                   : true,
-		"__non_webpack_require__" : true,
-		"__version__"             : true
-	},
-	"rules": {
-		"react/jsx-no-bind": ["error"],
-		"react/jsx-uses-vars": ["error"],
-		"react/jsx-uses-react": ["error"],
-		"react/prop-types": ["off"],
+  "env": {
+    "browser": true,
+    "commonjs": true,
+    "jquery": true,
+    "amd": true,
+    "es6": true,
+    "node": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended"
+  ],
+  "parser": "babel-eslint",
+  "parserOptions": {
+    "ecmaVersion": 8, // or 7
+    "ecmaFeatures": {
+      "jsx": true,
+      "experimentalObjectRestSpread": true
+    },
+    "sourceType": "module"
+  },
+  "plugins": [
+    'markdown',
+    'react',
+    'babel'
+  ],
+  "globals": {
+    "window": true,
+    "EJS": true,
+    "undefined": true,
+    "light": true,
+    "__non_webpack_require__": true,
+    "__version__": true
+  },
+  "rules": {
+    "react/jsx-no-bind": [ "error" ],
+    "react/jsx-uses-vars": [ "error" ],
+    "react/jsx-uses-react": [ "error" ],
+    "react/prop-types": [ "off" ],
     "no-case-declarations": [ "off" ],
-		"no-console": ["off"],
-		"no-empty": ["off"],
-		"indent": ["warn", 2, {"SwitchCase":1}],
-		"linebreak-style": ["error", "unix"],
-		"semi": ["warn", "always"]
-	}
+    "no-console": [ "off" ],
+    "no-empty": [ "off" ],
+    "indent": [ "warn", 2, { "SwitchCase": 1 } ],
+    "linebreak-style": [ "error", "unix" ],
+    "semi": [ "warn", "always" ]
+  }
 };
