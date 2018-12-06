4a85fad6fbd1d517e138d5f643cb1705c886c365
sfger <sfger111@163.com>
2018-12-06 20:07:25 +0800

    update

--- src/react/js/react.entry.jsx
+++ src/react/js/react.entry.jsx
@@ -31,9 +31,9 @@ class CommentBox extends Component {
     // 	nu:0
     // };
   }
-  componentWillMount() {
-    console.log( 'will', this );
-  }
+  // componentWillMount() {
+  //   console.log( 'will', this );
+  // }
   componentDidMount() {
     console.log( 'did', this );
   }
@@ -51,21 +51,21 @@ class CommentBox extends Component {
           <a href="javascript:" data-test="test">row:{row},a:{a},b:{b}</a>
         </div>
         <div>
-          <a href="javascript:" onClick={this.add_nu}>Add nu</a>
+          <a href="javascript:" onClick={ this.add_nu }>Add nu</a>
           <span>{this.state.nu}</span>
         </div>
         <div>
-          <a href="javascript:" onClick={add_num}>Add num</a>
+          <a href="javascript:" onClick={ add_num }>Add num</a>
           <span>{num}</span>
         </div>
         <div>
-          <a href="javascript:" onClick={list_push}>Push list</a>
+          <a href="javascript:" onClick={ list_push }>Push list</a>
           &nbsp;
-          <a href="javascript:" onClick={list_pop}>Pop list</a>
+          <a href="javascript:" onClick={ list_pop }>Pop list</a>
           <ul>
             {
               list.map(function(one){
-                return <li key={one}>{one}</li>;
+                return <li key={ one }>{one}</li>;
               })
             }
           </ul>
@@ -133,23 +133,23 @@ const ListShow = connect( mapStateToProps, mapDispatchToProps )( CommentBox );
 
 let test = { a: 'aaa', b: 'bbb' };
 ReactDOM.render(
-  <Provider store={store}>
-    <Router history={history}>
+  <Provider store={ store }>
+    <Router history={ history }>
       <div>
         <ul>
-          <li><Link to={location.pathname}>Home</Link></li>
+          <li><Link to={ location.pathname }>Home</Link></li>
           <li><Link to="/index">index</Link></li>
           <li><Link to="/test">test</Link></li>
         </ul>
-        <Route path={location.pathname} exact render={()=>{
-          return <ListShow row={'testRow'} {...test} />;
-        }}/>
-        <Route path="/index" render={()=>{
+        <Route path={ location.pathname } exact render={ ()=>{
+          return <ListShow row={ 'testRow' } { ...test } />;
+        } }/>
+        <Route path="/index" render={ ()=>{
           return <div>home</div>;
-        }}/>
-        <Route path="/test" render={()=>{
+        } }/>
+        <Route path="/test" render={ ()=>{
           return <div>hello world</div>;
-        }} />
+        } } />
       </div>
     </Router>
   </Provider>,
